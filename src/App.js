8ed371c030ea99066bccb700852d6c497cbdd8d6
Sami-Ullah-Tufail <sami.u.tufail@gmail.com>
2023-09-30 23:34:22 +0500

    fixing glitch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import {
   createBrowserRouter, createRoutesFromElements, RouterProvider, Route,
 } from 'react-router-dom';
-import Books from './components/pages/books';
-import Categories from './components/pages/categories';
-import Nav from './components/pages/nav';
+import Books from './components/pages/books.js';
+import Categories from './components/pages/categories.js';
+import Nav from './components/pages/nav.js';
 
 function App() {
   const router = createBrowserRouter(
